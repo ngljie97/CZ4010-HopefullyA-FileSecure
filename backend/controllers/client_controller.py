3901c99208d811e3698c041a7da26bb9af30b8c2
ngljie97 <ngl.jie97@gmail.com>
2021-10-11 12:39:54 +0800

    Updated client_controller for decryption and construction of file.

--- backend/controllers/client_controller.py
+++ backend/controllers/client_controller.py
@@ -1,5 +1,7 @@
 import os
 import socket
+
+from Crypto.Cipher import AES
 from backend.implementations.aescipher import AESCipher
 from backend.globals import SERVER_IP, SERVER_PORT, USER_TOKEN
 from backend.implementations.raid import Raid3Manager
@@ -7,31 +9,56 @@ from backend.implementations.raid import Raid3Manager
 # Client - Basic functionalities
 
 
-def secure_send(input_file, enc_key):  # To send a file to the server
+# Sending file to server
+def secure_send(input_file, enc_key):
 
-    # Splice and introduce redundancy - using RAID3 concepts
+    ## Splice and introduce redundancy - using RAID3 concepts
     filesize = os.path.getsize(input_file)
     raid = Raid3Manager(input_file=input_file, filesize=filesize)
     out_3paths = raid.compute_parity_hash()
 
-    # Encrypt the 3-parts file, each with a different nonce
+    ## Encrypt the 3-parts file, each with a different nonce
     trx_q = []  # queue for file transmission to the server
     crypt = AESCipher(enc_key)
     for file in out_3paths:
         trx_q.append(crypt.encrypt(file))
+        if os.path.exists(file):
+            os.remove(file)
 
-    # Send the file to the server from the queue, trx_q
-    ### To be implemented
+    ## Send the file to the server from the queue, trx_q
+    ### To be implemented (server should store userid, filesize, 3-part encrypted file)
+
+    ### Create database entry
+    ### Send file to server for storage
 
     return 0
 
 
-def secure_download(file_name, dest_dir):  # To download a file from the server
+# Download the file, file_name, from server to chosen directory, dest_dir.
+def secure_download(file_name, dest_dir, prikey_dir):
 
-    # Download the file, file_name, from server to chosen directory, dest_dir
+    ## Receive 3 files from server to the dest_dir
     ### To be implemented
 
-    
+    ## Retrieve file information from database
+    filesize = 99
+    host_user = ''
+    host_pubkey = ''
+
+    ## Exchange and decrypt the key for decryption
+    dec_key = ''
+
+    ## Decrypt the 3-parts using the key obtained from the exchange
+    crypt = AESCipher(dec_key)
+    file = os.path.join(dest_dir, file_name)
+
+    for i in range(1, 4):
+        crypt.decrypt(file + '.p' + str(i) + '.enc')
+
+    ## Joins the 3 parts
+    file_p1 = file + '.p1'
+    raid = Raid3Manager(filesize=filesize, input_file=file_p1)
+    raid.check_and_construct()
     return 0
 
 
