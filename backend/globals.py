73c31693f4d2952366fe6fca5dded4b05564b8bf
ngljie97 <ngl.jie97@gmail.com>
2021-11-27 22:51:29 +0800

    DONEEEEE

--- backend/globals.py
+++ backend/globals.py
@@ -14,22 +14,22 @@ def init():
     global FIREBASE_CONN
 
     ## ==================Change this section to your own configurations ============================
-    SERVER_IP = '192.168.33.63'
+    SERVER_IP = '192.168.33.79'
     SERVER_PORT = 65432
     ## ======================End of section to be editted===========================================
 
     # The following variables do not have to be changed. Do it at your own risk!
     PROJ_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-    __PATH_TO_KEY = os.path.join(PROJ_ROOT, 'backend', 'secret',
-                                 'firebase-privatekey.json')
     MAX_LOGIN_ATTEMPTS = 3
     AUTH_USER = {}
     IS_AUTHENTICATED = False
     FIREBASE_CONN = pyrebase.initialize_app({
-        "apiKey": "AIzaSyBd9GRTbL_lIZyJtpQkUfvGwfXmzwuDszc",
-        "authDomain": "cz4010fs.firebaseapp.com",
+        "apiKey":
+        "AIzaSyBd9GRTbL_lIZyJtpQkUfvGwfXmzwuDszc",
+        "authDomain":
+        "cz4010fs.firebaseapp.com",
         "databaseURL":
         "https://cz4010fs-default-rtdb.asia-southeast1.firebasedatabase.app/",
-        "storageBucket": "cz4010fs.appspot.com",
-        "serviceAccount": __PATH_TO_KEY
+        "storageBucket":
+        "cz4010fs.appspot.com"
     })
